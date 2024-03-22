1f829c717675658b5cf1c6f8313ac24956d01d04
Thomas Santerre <thomas@santerre.xyz>
2024-03-22 13:03:03 -0400

    adjust benchmark

--- candle-core/benches/benchmarks/index_select.rs
+++ candle-core/benches/benchmarks/index_select.rs
@@ -10,18 +10,18 @@ fn run(x: &Tensor, ids: &Tensor, dim: usize) {
 fn run_benchmark(c: &mut Criterion, device: &Device, dtype: DType, name: &str) {
     let t = Tensor::arange(0.0f32, 10000.0, device)
         .unwrap()
-        .reshape((1, 4, 50, 50))
+        .reshape((1, 1, 50, 200))
         .unwrap()
         .to_dtype(dtype)
         .unwrap();
 
-    let ids = Tensor::new(&[0u8, 100, 203], device).unwrap();
+    let ids = Tensor::new(&[0u8, 100, 199], device).unwrap();
 
     let flops = t.dims().iter().product::<usize>() * dtype.size_in_bytes();
 
     let mut group = c.benchmark_group(device.bench_name(name));
     group.throughput(Throughput::Bytes(flops as u64));
-    group.bench_function("iter", move |b| {
+    group.bench_function("index_select", move |b| {
         b.iter_custom(|iters| {
             let start = Instant::now();
             for _i in 0..iters {
